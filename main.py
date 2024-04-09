d20a6d0dfdbf549b4161d2803abca47424a45e3e
jblkea <jblkea9001@email.com>
2024-04-09 23:02:02 +0800

    Day28: Add date on the header. Change to image.png.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+from datetime import datetime
 
 api_key = "kO5DBJBV7BQfF6kAnxiY0GAwPqCg7u0xFgvjQRxH"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
@@ -13,6 +14,10 @@ title_of_the_day = content["title"]
 explanation = content["explanation"]
 pic_url = content["url"]
 
+# Extract the date and convert to DD-MMM-YYYY
+ExtractDate = datetime.strptime(content["date"], '%Y-%m-%d')
+ExtractDate = ExtractDate.strftime('%d-%b-%Y').upper()
+
 # Download the image and save in image.jpg
 pic_bytes = requests.get(pic_url)
 with open("image.png", "wb") as file:
@@ -20,6 +25,7 @@ with open("image.png", "wb") as file:
 
 # Start rendering webpage
 # Display Title
+st.title(ExtractDate + ':')
 st.title(title_of_the_day)
 
 # Display Picture
