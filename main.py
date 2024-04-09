0b65c43f9e1b1abfc9860aaac8ada4437c78b6ba
jblkea <jblkea9001@email.com>
2024-04-09 22:46:22 +0800

    Day28: Change image.jpg to image.png. Change to st.title instead of header.

--- main.py
+++ main.py
@@ -4,22 +4,26 @@ import streamlit as st
 api_key = "kO5DBJBV7BQfF6kAnxiY0GAwPqCg7u0xFgvjQRxH"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
-# Request NASA for picture of the day, and title and save in image.jpg
+# Get the request data as a dictionary
 request = requests.get(url)
 content = request.json()
+
+# Extract the image url, title and explanation
 title_of_the_day = content["title"]
 explanation = content["explanation"]
 pic_url = content["url"]
+
+# Download the image and save in image.jpg
 pic_bytes = requests.get(pic_url)
-with open("image.jpg", "wb") as file:
+with open("image.png", "wb") as file:
     file.write(pic_bytes.content)
 
 # Start rendering webpage
 # Display Title
-st.header(title_of_the_day)
+st.title(title_of_the_day)
 
 # Display Picture
-st.image("image.jpg")
+st.image("image.png")
 
 # Display Description
 st.write(explanation)
